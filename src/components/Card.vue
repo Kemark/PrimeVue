b10b595884151062b536c4dea59e441e36bb3f8d
Kemark <markus@mail-kern.de>
2024-08-05 09:15:44 +0200

    Initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <VueCard>
+    <template #title>Simple Card</template>
+    <template #content>
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </p>
+    </template>
+  </VueCard>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import VueCard from 'primevue/card';
+</script>
+
+<style scoped></style>
